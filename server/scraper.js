212afa1e278d58ab49be721424ef8f7209a1c7ca
Paul <paul.iaszfalvi@gmail.com>
2023-09-15 15:52:20 -0400

    Scraper and server working

--- server/scraper.js
+++ server/scraper.js
@@ -1,56 +1,38 @@
-const puppeteer = require('puppeteer');
 const axios = require('axios'); 
-
 
 async function fetchData() {
   try {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    const response = await axios.get('https://www.reddit.com/r/programming.json');
+    const jsonData = response.data;
 
-    // Navigate to the Reddit URL
-    await page.goto('https://www.reddit.com/r/programming.json');
-
-    // Wait for the page to load completely (you can adjust the timeout as needed)
-    await page.waitForSelector('body');
-
-    // Extract the JSON data from the page
-    const jsonData = await page.evaluate(() => {
-      return JSON.parse(document.querySelector('pre').textContent);
-    });
-
-    // Close the browser
-    await browser.close();
-
-    let myArray = [];
-
-    // Extract the title from the JSON data
-    for (let i = 0; i < 15; i++) {
-      let title = jsonData.data.children[i].data.title;
-      let user = jsonData.data.children[i].data.author;
-      let score = jsonData.data.children[i].data.score;
-      const permalink = jsonData.data.children[i].data.permalink;
-      let selftext = jsonData.data.children[i].data.selftext;
-      let sourceLink = jsonData.data.children[i].data.url;
+    const myArray = jsonData.data.children.slice(0, 15).map(child => {
+      let childData = child.data;
+      let title = childData ? childData.title : undefined;
+      let user = childData ? childData.author : undefined;
+      let score = childData ? childData.score : undefined;
+      const permalink = childData ? childData.permalink : undefined;
+      let selftext = childData ? childData.selftext : undefined;
+      let sourceLink = childData ? childData.url : undefined;
       // If the post isn't a discussion, return the link to what it points to instead of the discussion
       if (selftext === "") {
-        selftext = jsonData.data.children[i].data.url;
+        selftext = childData ? childData.url : undefined;
       }
 
-      myArray.push({
+      return ({
         title: title,
         user: user,
         score: score,
         selfText: selftext,
         sourceLink: sourceLink,
       });
-
-      console.log(myArray)
-      return JSON.stringify(myArray)     
-    }
-    
+    });
+    console.log(myArray)
+    return myArray; // Return the array outside the for loop
   } catch (error) {
     console.error('Error:', error);
+    throw error; // Throw the error to handle it outside this function
   }
-};
+}
 
-module.exports = {fetchData};
+module.exports = { fetchData };
+
--- server/server.js
+++ server/server.js
@@ -1,54 +1,27 @@
 const express = require("express");
-const { stringify } = require("nodemon/lib/utils");
 const scraper = require("./scraper");
-const router = express.Router();
 const app = express();
 const PORT = process.env.PORT || 5000;
-var favicon = require("serve-favicon");
-var path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose')
 
 // Configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-//fetch(request, { mode: "cors" });
-
-//app.use(favicon(path.join(__dirname, "../client/public/network.png")));
 
 app.use(express.static('public'))
 
 app.get("/info", async (req, res) => {
-  console.log(await scraper); //, new Date().getTime());
-  
-  res.json(await scraper);
-
+  try {
+    const data = await scraper.fetchData(); // Call the fetchData function
+    res.json(data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
 });
-
-app.get("/info", async (req, res) => {
-  const myArray = scraper
-  console.log(myArray)
-  res.send(myArray)  
-});
-
-// app.post("sendData", async (req,res) => {
-//   const {myArray} = req.body
-
-//   try {
-
-//     await scraper.insertMany([myArray = myArray]) 
-
-//   }
-//   catch(e) {
-//     console.log(e)
-//   }
-// })
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
-
-module.exports = app;
-module.exports = router;
