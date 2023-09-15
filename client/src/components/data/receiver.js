7dc23a8fea1451fb5bbda14455d8cf7965d8e656
Paul <paul.iaszfalvi@gmail.com>
2023-09-15 16:38:59 -0400

    Got front end working with data

--- client/src/components/data/receiver.js
+++ client/src/components/data/receiver.js
@@ -1,30 +1,41 @@
-import React, { Component, useEffect, useState } from 'react'
-import axios from "axios"
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function Receiver() {
+const Receiver = () => {
+  const [data, setData] = useState([]);
 
-    const [data, setData] = useState(null);
+  useEffect(() => {
+    // Define the API endpoint
+    const apiUrl = 'http://localhost:5000/info';
 
-
-    const fetchNotes = async () => {
-        const { data } = await axios.get(`/info`);
-        console.log(data)
-    }
-
-    useEffect(() => {
-        fetchNotes()   
-}, []); 
-
-
-
+    // Fetch data from the server
+    axios.get(apiUrl)
+      .then(response => {
+        setData(response.data);
+        console.log(response.data)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });      
+  }, []);
 
   return (
-    <div>
-
-       
-      
+    <div>    
+      <h2>Data Fetcher Component</h2>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            <strong>Title:</strong> {item.title}<br />
+            <strong>Score:</strong> {item.score}<br />
+            <strong>Permalink:</strong> {item.sourceLink}<br />
+            <strong>Selftext:</strong> {item.selfText}<br />
+            <strong>User:</strong> {item.user}<br />
+            <br></br>
+          </li>         
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Receiver;
