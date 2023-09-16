6069520892d105d07796408ae5f75d022ddda4ae
Paul <paul.iaszfalvi@gmail.com>
2023-09-15 20:58:49 -0400

    Fixed bugged permalink

--- server/scraper.js
+++ server/scraper.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 async function fetchData() {
   try {
-    const response = await axios.get('https://www.reddit.com/r/programming.json');
+    const response = await axios.get('https://www.reddit.com/r/learnprogramming.json');
     const jsonData = response.data;
 
     const myArray = jsonData.data.children.slice(0, 15).map(child => {
@@ -10,7 +10,7 @@ async function fetchData() {
       let title = childData ? childData.title : undefined;
       let user = childData ? childData.author : undefined;
       let score = childData ? childData.score : undefined;
-      const permaLink = childData ? "www.reddit.com" + childData.permalink : undefined;
+      const permaLink = childData ? `https://www.reddit.com${childData.permalink}` : undefined;
       let selftext = childData ? childData.selftext : undefined;
       let sourceLink = childData ? childData.url : undefined;
       // If the post isn't a discussion, return the link to what it points to instead of the discussion
