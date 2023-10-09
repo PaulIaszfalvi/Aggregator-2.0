82403b0e28adaff85b70a6178d0197273983b31f
paul <paul.iaszfalvi@gmail.com>
2023-10-09 14:53:24 -0400

    checking dbURI is set to private on gitignore

--- server/server.js
+++ server/server.js
@@ -4,6 +4,34 @@ const scraper = require("./scraper");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const dbURI = require("./textFiles/dbURI.json")
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Handle connection events
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('MongoDB disconnected');
+});
+
+// Close the MongoDB connection when your Node.js application exits
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('MongoDB connection closed through app termination');
+    process.exit(0);
+  });
+});
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
